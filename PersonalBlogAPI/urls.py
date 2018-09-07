ca7d0903d0de7aca38d148c53a311b85621585e8
Chase Weyer <cpwfe87@gmail.com>
2018-09-07 18:33:17 -0400

    Updated url paths

--- PersonalBlogAPI/urls.py
+++ PersonalBlogAPI/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Include URL's for API to make sure app routes to API
-    path('', include('blogAPI.urls'))
+    path('api/', include('blogAPI.urls'))
 ]
