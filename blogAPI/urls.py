a24152abb435eb1d357d9fb61e12e7bdd8ce96d3
Chase Weyer <cpwfe87@gmail.com>
2018-09-07 18:23:10 -0400

    Updated urls

--- blogAPI/urls.py
+++ blogAPI/urls.py
@@ -2,14 +2,14 @@
 
 from django.urls import *
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import BlogCreateView, BlogDetailsView, PageCreateView
+from .views import BlogCreateView, BlogDetailsView, PageCreateView, PageDetailsView
 
 # Set patters for specific urls
 urlpatterns = {
     path('posts/', BlogCreateView.as_view(), name="posts"),
     path('posts/<slug>/', BlogDetailsView.as_view(), name="blog-details"),
     path('pages/', PageCreateView.as_view(), name="pages"),
-    path('pages/<title>', PageCreateView.as_view(), name="pages-details")
+    path('pages/<title>', PageDetailsView.as_view(), name="pages-details")
 }
 
 # Format url based on url patterns
